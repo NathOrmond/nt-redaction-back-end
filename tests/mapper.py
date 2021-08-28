2cdb03e79dcb7748f2170fdc315d73a7f30b66e1
vvveracruz <login@vgg.cz>
2021-08-28 11:12:24 +0100

    tests for get distances wrapper

--- tests/mapper.py
+++ tests/mapper.py
@@ -1,6 +1,10 @@
 import os
-from redactioncalculator.mapper import mapper, text_to_wordlist
+from redactioncalculator.mapper import mapper, file_to_wordlist, unmapper
 
 if __name__=="__main__":
-    wordlist=text_to_wordlist(os.getcwd()+'/tests/a_sentence.txt')
-    print(mapper(wordlist))
+    wordlist=file_to_wordlist(os.getcwd()+'/tests/a_sentence.txt')
+    mapped_text, mapping_dict=mapper(wordlist)
+    unmapped=unmapper(mapping_dict, mapped_text)
+    print(mapped_text)
+    print(unmapped)
+    print(wordlist==unmapped)
